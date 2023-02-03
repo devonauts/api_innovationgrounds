a1bde7617a0d66b7c3292c706979a8046edcc726
N1samar <124205895+N1samar@users.noreply.github.com>
2023-02-03 16:27:48 -0500

    Update user.js

--- SRC/ROUTES/user.js
+++ SRC/ROUTES/user.js
@@ -1,14 +1,65 @@
 // conexiones 
 const express = require('express');
-const {SELECT}  = require('sequelize/types/query-types');
 const sqlcon = require('../DB/database');
 const router = express.Router();
 
 // rutas de archivo 
+
 router.get('/',(req,res)=>{
-    sqlcon.query('SELECT *FROM USER',(err, rows, fields)=>{
+    sqlcon.query('SELECT * FROM USER',(err, rows, fields)=>{
         if(!err){
             res.json(rows);
+        }else{
+            console.log(err);
+        }
+    });
+});
+router.get('/:id',(req,res)=>{
+   const {id} = req.params;
+   sqlcon.query('SELECT * FROM user WHERE id = ? ', [id], (err, rows, fields)=>{
+        if(!err){
+            res.json(rows[0]);
+        }else{
+            console.log(err);
+        }
+    });
+});
+router.post('/', (req, res)=>{
+    const {id, name, address, email, phone_num} = req.body;
+    console.log(req.body)
+    const query= `
+        CALL userEdit(?, ?, ?, ?, ?);
+    `;
+    sqlcon.query(query,[id, name, address, email, phone_num], (err, rows, fields)=>{
+        if(!err){
+            res.json({status: 'User saved'});
+        }else{
+            console.log(err);
+        }
+    });
+
+});
+router.put('/:id', (req, res)=>{
+    const { name, address, email, phone_num} = req.body;
+    const {id} = req.params;
+    const query= `
+        CALL userEdit(?, ?, ?, ?, ?);
+    `;
+    sqlcon.query(query,[id, name, address, email, phone_num], (err, rows, fields)=>{
+        if(!err){
+            res.json({status: 'User updated'});
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+router.delete('/:id', (req, res)=>{
+    const { name, address, email, phone_num} = req.body;
+    const {id} = req.params;
+    sqlcon.query('DELETE * FROM user WHERE id = ?',[id], (err, rows, fields)=>{
+        if(!err){
+            res.json({status: 'Deleted'});
         }else{
             console.log(err);
         }
