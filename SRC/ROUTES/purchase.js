151e712176447b400008489d4a5f8c9483bbb5fb
N1samar <124205895+N1samar@users.noreply.github.com>
2023-02-14 10:28:36 -0500

    Update purchase.js

--- SRC/ROUTES/purchase.js
+++ SRC/ROUTES/purchase.js
@@ -1,6 +1,33 @@
-const express = require("express");
-const sqlcon = require('../DB/database');
+const express = require('express');
 const router = express.Router();
+const purchase = require('./MODELS/purchase.js');
+
+router.get('/', async (req,res)=>{
+    const purchase =  await purchase.getAll();
+    res.json(result);
+});
+
+router.get('/id', async (req,res)=>{
+    const purchase =  await purchase.getId(req.body.id);
+    res.json(result);
+});
+
+router.get('/date', async (req,res)=>{
+    const purchase =  await purchase.getDate(req.body.id);
+    res.json(result);
+});
+
+router.get('/request', async (req, res)=>{
+    console.log(req.body);
+    const services = await services.request();
+    res.json(result);
+});
+
+router.get('/delete', async (req, res)=>{
+    console.log(req.body);
+    const purchase = await purchase.quit();
+    res.json(result);
+});
 
 
-module.exports = router
+module.exports = router;
